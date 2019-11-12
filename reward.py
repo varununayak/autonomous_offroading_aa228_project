f143a9fa5112a8ab9d829ede376a5540fe269f6c
Pranav Keni <pranavkeni@Pranavs-MacBook-Pro.local>
2019-11-12 14:27:42 -0800

    Reward function first pass

---
+++ reward.py
@@ -0,0 +1,11 @@
+#Function for the reward model
+
+def CalculateReward(Snext_t, Vnext):
+	#Snext_t is the next state vector which has [Del2H, d_goal]
+	#Weights for the terms in reward
+	a1 = 0.5
+	a2 = 0.5
+	d_goal = Snext_t[1]
+	Del2H = Snext_t[0]
+	reward = a1*d_goal*np.abs(Vnext) + a2*Del2H/np.abs(Vnext)
+	return reward
