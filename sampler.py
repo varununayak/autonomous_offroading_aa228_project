5999e31ba4d8d3d64cd6e0899285a21cfa717a9a
kshitijkumbar <kshitij.kumbar@outlook.com>
2019-11-12 15:25:43 -0800

    added CSV generator

--- sampler.py
+++ sampler.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pathGenerator.py import generateRandomPath
+from pathGenerator import *
+from policyGenerator import *
+from reward import *
 '''
 DATA SAMPLER
 
@@ -14,7 +16,7 @@ def sample_generator(pathData,velocityData):
 	R = []
 	Sp = []
 
-	for index in range(len(path)-1):
+	for index in range(len(pathArray)-1):
 		s_next = [pathArray[index+1],(totalLength-((index+1)*stepSize))]
 		v_next = velocityData[index+1]
 		current_reward = CalculateReward(s_next, v_next)
@@ -23,10 +25,21 @@ def sample_generator(pathData,velocityData):
 		R.append(current_reward)
 		Sp.append(s_next)
 
+	print(np.shape(S),np.shape(A),np.shape(R),np.shape(Sp))
+
+	S = np.array(S)
+	Sp = np.array(Sp)
+
+	DataCombined = np.transpose(np.vstack((S[:,0], S[:,1],A,R,Sp[:, 0], Sp[:, 1])))
+	np.savetxt("samples.csv", DataCombined, delimiter=",")
+
+	# for 
+
 
 
 def main():
 	pathData = generateRandomPath()
+	velocityData = generateConstantVelocityProfile(1)
 	sample_generator(pathData,velocityData)
 
 
