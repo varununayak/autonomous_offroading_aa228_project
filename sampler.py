6f7d83b0b1ba2fb106f2056760a1e64a899b5093
kshitijkumbar <kshitij.kumbar@outlook.com>
2019-11-12 15:07:49 -0800

    updated with SARSp vals

--- sampler.py
+++ sampler.py
@@ -1,16 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pathGenerator.py import generateRandomPath
 '''
 DATA SAMPLER
 
 This program take a path and reward function and returns samples to be used for learning
 '''
 
-def sample_generator(pathData):
-	pathArray,stepSize,totalLength
+def sample_generator(pathData,velocityData):
+	pathArray,stepSize,totalLength = pathData
 	S = []
 	A = []
 	R = []
-	S = 
+	Sp = []
 
-	for index in range(len(path)):
+	for index in range(len(path)-1):
+		s_next = [pathArray[index+1],(totalLength-((index+1)*stepSize))]
+		v_next = velocityData[index+1]
+		current_reward = CalculateReward(s_next, v_next)
+		S.append([pathArray[index],(totalLength-((index)*stepSize))])
+		A.append(velocityData[index+1])
+		R.append(current_reward)
+		Sp.append(s_next)
+
+
+
+def main():
+	pathData = generateRandomPath()
+	sample_generator(pathData,velocityData)
+
+
+if __name__	== "__main__":
+	main()
