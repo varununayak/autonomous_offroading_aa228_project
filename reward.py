c4c697f3d9911c3a65f15ee83855f1c9b6e9a88e
kshitijkumbar <kshitij.kumbar@outlook.com>
2019-11-30 18:43:20 -0800

    reward funnction augmented with next grad

--- reward.py
+++ reward.py
@@ -3,12 +3,18 @@ import numpy as np
 
 def CalculateReward(s, a):
     usingKshitijsFunction = True
+    Del2H1 = s[1]
+    Del2H2 = s[2]
+    curr_vel = s[0]
     d_goal = s[3]
-    Del2H = s[1]
     if (usingKshitijsFunction):
         a1 = 0.5
-        a2 = 0.9
-        reward = a1*d_goal*np.abs(a) - (a2*np.abs(Del2H))/np.abs(a)
+        a2 = 1.0
+        a3 = 0.8
+        if curr_vel == 0:
+            return 0
+        # penalized current velocity as well as intended velocity using the current and next gradient
+        reward = a1*d_goal*np.abs(a) - (a2*np.abs(Del2H1))/np.abs(curr_vel) - (a3*np.abs(Del2H2))/np.abs(a)
     else:
         a1 = 0.05
         a2 = 0.5
